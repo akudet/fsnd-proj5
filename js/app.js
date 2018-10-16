fb7141bf67c96f3aa5d7bb97d75b5866f7af3eb6
Jintong Wu <akudet@qq.com>
2018-10-16 22:57:24 +0800

    show loc detail when user click a pushin or loc item

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
+/**
+ * model part of our up, it will model Location, and use a third party lib to get
+ * more info about a location
+ * @param map Map class in Bing Maps V8 Map Control
+ * @returns {AppModel}
+ * @constructor
+ */
 function AppModel(map) {
+    /**
+     * location model of our app
+     * @param name name of the location
+     * @param latitude latitude of the location
+     * @param longitude longitude of the location
+     * @returns {Location}
+     * @constructor
+     */
     function Location(name, latitude, longitude) {
         let that = this;
 
@@ -12,7 +27,7 @@ function AppModel(map) {
     const that = this;
 
     that.map = map;
-    that.randomLocations = function (func) {
+    that.randomLocations = function () {
         const numOfPts = 7;
         const points = Microsoft.Maps.TestDataGenerator.getLocations(numOfPts, that.map.getBounds());
         let i = 0;
@@ -24,7 +39,7 @@ function AppModel(map) {
         points.forEach(function (point) {
             locations.push(new Location(names[i++], point.latitude, point.longitude));
         });
-        func(locations);
+        return locations;
     };
 
     return that;
@@ -35,15 +50,15 @@ function AppViewModel(map) {
     const that = this;
     const model = new AppModel(map);
 
+    // the keyword user may want to use to filter locations
     that.keyword = ko.observable("");
-    that.locations = ko.observableArray([]);
-
-    model.randomLocations(function (locations) {
-        locations.forEach(function (loc) {
-            that.locations().push(loc);
-        });
-    });
-
+    // all locations out model will have
+    that.locations = ko.observableArray(model.randomLocations());
+    // the location user may interested(which location will show a detailed info box)
+    // it will be set to the location that user click in the list location view, or a pushin in map
+    that.currLocation = ko.observable(null);
+    // subset of all locations which we what to show
+    // currently it will be shown using a list view and pushins in map
     that.filteredLocations = ko.computed(function () {
         let keyword = that.keyword().toLowerCase();
         return ko.utils.arrayFilter(that.locations(), function (location) {
@@ -51,18 +66,46 @@ function AppViewModel(map) {
         });
     }, that);
 
+    // infobox used to show location detail
+    const infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
+        visible: false
+    });
+    infobox.setMap(map);
+
+    that.currLocation.subscribe(function (newValue) {
+        if (newValue) {
+            let pt = new Microsoft.Maps.Location(newValue.latitude, newValue.longitude);
+            infobox.setOptions({
+                location: pt,
+                title: newValue.name,
+                visible: true
+            });
+        }
+    });
+
+    that.locationItemClicked = function (loc) {
+        that.currLocation(loc);
+    };
+
+    function pushpinClicked(e) {
+        that.currLocation(e.target.metadata.locationItem);
+    }
+
+    // update pushins when filtered locations changed
     let updatePushins = function () {
         map.entities.clear();
         that.filteredLocations().forEach(function (loc) {
             let pushin = new Microsoft.Maps.Pushpin(loc);
+            pushin.metadata = {
+                locationItem: loc
+            };
             map.entities.push(pushin);
+            Microsoft.Maps.Events.addHandler(pushin, 'click', pushpinClicked);
         })
     };
-
     that.filteredLocations.subscribe(function () {
         updatePushins();
     });
-
     updatePushins();
 }
 
